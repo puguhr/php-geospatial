877d5ef22205999a0c6ddbd91a6f1b9c908b0270
puguhr <puguhr@gmail.com>
2025-06-29 21:39:57 +0800

    Create example.php

---
+++ example.php
@@ -0,0 +1,28 @@
+<form method="post" enctype="multipart/form-data">
+	<input type="file" name="userfile" required> 
+	<input type="submit">
+</form>
+
+<?php
+if (!empty($_FILES['userfile'])) {
+  require_once "PHPGeospatial.php";
+	$obj = new PHPGeospatial();
+	$obj->fromGeojson(file_get_contents($_FILES['userfile']['tmp_name']));
+
+  // get geospatial type
+  echo "Geospatial type : " . $obj->getType() . "<br>";
+
+  // get features properties
+  // return as array of feature properties, if only one feature it return the first feature
+  echo "Features : <pre>" . json_eoncode($obj->getProperties()) . "</pre> <br>";
+
+  // get sql string to insert to database table
+  // return as array of feature properties, if only one feature it return the first feature
+  // first parameter is the name of the table
+  // the second is for geometry field name
+  // the third is array for additional fields, for example array('updated_on'=>date('Y-m-d H:i:s'))
+  // the fourth is for the the insert method : INSERT, INSERT_IGNORE, INSERT_UPDATE
+  echo "SQL : <pre>" . json_eoncode($obj->insertSql('tableName', 'geometry', array(), $obj::INSERT)) . "</pre> <br>";
+
+}
+?>
